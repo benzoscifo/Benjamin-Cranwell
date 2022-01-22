63a5165c0eeec5db5b84c6e05b88e358885178aa
benjamin cranwell <benzoscifo@yahoo.com>
2022-01-22 08:05:46 +0000

    add contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,25 +7,60 @@ export default function contact() {
       <section className="text-gray-700">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="md:w-1/2 lg:pr-24 md:pr-16 md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <form name="contact" method="POST" data-netlify="true">
-              <p>
-                <label>
-                  Your Name: <input type="text" name="name" />
+            <form
+              accept-charset="UTF-8"
+              action="https://getform.io/f/b9a6314e-2785-4a82-b122-b58dea000ee7"
+              method="POST"
+              target="_blank"
+            >
+              <div class="form-group">
+                <label for="exampleInputName">Full Name</label>
+                <input
+                  type="text"
+                  name="fullname"
+                  class="form-control"
+                  id="exampleInputName"
+                  placeholder="Enter your name and surname"
+                  required="required"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleInputEmail1" required="required">
+                  Email address
                 </label>
-              </p>
-              <p>
-                <label>
-                  Your Email: <input type="email" name="email" />
+                <input
+                  type="email"
+                  name="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div class="form-group">
+                <label for="exampleFormControlSelect1">
+                  Favourite Platform
                 </label>
-              </p>
-              <p>
-                <label>
-                  Message: <textarea name="message"></textarea>
-                </label>
-              </p>
-              <p>
-                <button type="submit">Send</button>
-              </p>
+                <select
+                  class="form-control"
+                  id="exampleFormControlSelect1"
+                  name="platform"
+                  required="required"
+                >
+                  <option>Github</option>
+                  <option>Gitlab</option>
+                  <option>Bitbucket</option>
+                </select>
+              </div>
+
+              <div class="form-group mt-3">
+                <label class="mr-2">Upload your CV:</label>
+                <input type="file" name="file" />
+              </div>
+
+              <button type="submit" class="btn btn-primary">
+                Submit
+              </button>
             </form>
           </div>
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
