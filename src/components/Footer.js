c5c81deed06d57e180eec80f40aabb4c0df7a509
benjamin cranwell <benzoscifo@yahoo.com>
2022-02-25 16:00:24 +0000

    add footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,38 +3,23 @@ import React from "react"
 
 export default function Footer() {
   return (
-    <div className="bg-gray-800 pt-6 md:pt-12">
-      <div className="container px-4 mx-auto">
-        <div className="md:flex md:flex-wrap md:-mx-4 py-6 md:pb-12">
-          <div className="footer-info lg:w-1/3 md:px-4">
-            <h4 className="text-white text-2xl mb-4">sundown</h4>
-          </div>
-          <div className="md:w-2/3 lg:w-1/3 md:px-4 xl:pl-16 mt-12 lg:mt-0">
-            <div className="sm:flex">
-              <div className="sm:flex-1">
-                <h5 className="text-base font-medium text-white uppercase mb-2">
-                  About
-                </h5>
-                <Link
-                  className="text-gray-400 py-1 block hover:underline"
-                  to="/about"
-                >
-                  Resume
-                </Link>
-              </div>
-              <div className="sm:flex-1 mt-4 sm:mt-0">
-                <h5 className="text-base font-medium text-white uppercase mb-2">
-                  Blog
-                </h5>
-                <Link
-                  className="text-gray-400 py-1 block hover:underline"
-                  to="/about"
-                >
-                  Company
-                </Link>
-              </div>
-            </div>
-          </div>
+    <div className="container px-4 py-12 mx-auto text-gray-400">
+      <div className="flex justify-around">
+        <div>
+          <a
+            href="https://www.linkedin.com/in/benjamin-cranwell-5945101a3"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://medium.com/@benjamin_51824/developer-to-ux-designer-portfolio-d508cdbfeb6a"
+            target="_blank"
+          >
+            Blog
+          </a>
         </div>
       </div>
     </div>
