e0037167b52e6cebfd08ccd00e387656c605cccd
benjamin cranwell <benzoscifo@yahoo.com>
2022-02-25 17:04:07 +0000

    add padding to heading

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       <section className="text-gray-700 body-font">
         <div className="container mx-auto flex px-5 pt-24 pb-20 md:flex-row flex-col items-center">
           <div className="w-full lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className=" sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+            <h1 className=" sm:text-4xl text-3xl mb-8 font-medium text-gray-900">
               I build web pages to be sleek, sharp and sustainable
             </h1>
             <div className="flex w-full lg:w-1/5 justify-center">
