71758f4126484d1132a42b5c7fe19c19388775aa
benjamin cranwell <benzoscifo@yahoo.com>
2022-02-28 13:32:03 +0000

    add rough notation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import Layout from "../components/Layout"
 import { StaticImage } from "gatsby-plugin-image"
 import { Link } from "gatsby"
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation"
 
-export default function Home() {
+const Home = () => {
+  const [show, setShow] = useState(true)
   return (
     <Layout>
       <section className="text-gray-700 body-font">
         <div className="container mx-auto flex px-5 pt-24 pb-20 md:flex-row flex-col items-center">
           <div className="w-full lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className=" sm:text-4xl text-3xl mb-8 font-medium text-gray-900">
-              I build web pages to be sleek, sharp and sustainable
+              I build web pages to be sleek, sharp and{" "}
+              <RoughNotation type="underline" show={show} color={"red"}>
+                sustainable
+              </RoughNotation>
             </h1>
-            <div className="animate-pulse flex w-full lg:w-1/5 justify-center">
+
+            <div className="flex w-full lg:w-1/5 justify-center">
               <Link
                 className="block w-full text-center text-green hover:text-orange border-2 border-green py-2 px-6 hover:border-orange rounded text-lg"
                 to="/caseStudies"
@@ -145,3 +151,5 @@ export default function Home() {
     </Layout>
   )
 }
+
+export default Home
