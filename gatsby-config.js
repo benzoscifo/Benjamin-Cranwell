16cfb1cc9500f61b53f9b64e18ddbd991f1b99e8
benjamin cranwell <benzoscifo@yahoo.com>
2022-03-06 16:22:28 +0000

    add new image

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,6 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
